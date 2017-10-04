ccd1cd89c1e97b05e951dacfc4df751bf5de4395
JFM44 <ib@192.168.162.11>
2017-10-04 11:40:03 +0200

    modif Login

--- Vues/ajoute_film.php
+++ Vues/ajoute_film.php
@@ -2,7 +2,7 @@
 // put your code here
 ?>
 
-<H1>Page en construction</H1>
+<H1>Ajout Film</H1>
 <FORM method="POST" action="front_controler.php?action=ajoute_film_post">
     <label> Nom du film </label> 
     <input name="nomFilm"/>
--- Vues/liste_films.php
+++ Vues/liste_films.php
@@ -24,7 +24,8 @@
             {
                 For($i=0;$i<count($ensFilms);$i++)
                 { 
-                    printf("<TR><TD> %d - %s </TD> <TD><INPUT TYPE=\"submit\" NAME=\"btSupprimer\" VALUE=\"Supprimer\"></TD></TR>",$i+1,$ensFilms[$i]);
+                    printf("<TR><TD> %d - %s </TD> <TD>");
+                    printf("<INPUT TYPE=\"submit\" NAME=\"btSupprimer\" VALUE=\"Supprimer\"></TD></TR>",$i+1,$ensFilms[$i]);
                 }
             }
             else
@@ -37,6 +38,6 @@
     }
 
 ?>
-         
+       <BUTTON
 </TABLE>
 
